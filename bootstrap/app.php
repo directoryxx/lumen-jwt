809cb3f884526f898788dd57a90b8aeafb6329d2
directoryx <anggawijaya4567@gmail.com>
2020-06-14 20:25:28 +0700

    Revert "upgrade 5.5 to 5.6"
    This reverts commit 4d5343f034405c94fb7ffb63a1daede717bf04ea.
    
    # Conflicts:
    #	composer.json

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,9 +23,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 */
 
 try {
-    (new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
-        dirname(__DIR__)
-    ))->bootstrap();
+    (new Dotenv\Dotenv(__DIR__.'/../'))->load();
 } catch (Dotenv\Exception\InvalidPathException $e) {
     //
 }
