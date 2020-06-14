93ed259ec6be7b319cb62e02a2f96e167ea1d579
directoryx <anggawijaya4567@gmail.com>
2020-06-14 18:40:02 +0700

    upgrade 5.4 to 5.5

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -119,8 +119,10 @@ $app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 |
 */
 
-$app->group(['namespace' => 'App\Http\Controllers'], function ($app) {
-    require __DIR__.'/../routes/api.php';
+$app->router->group([
+    'namespace' => 'App\Http\Controllers',
+], function ($router) {
+    require __DIR__.'/../routes/web.php';
 });
 
 return $app;
--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,11 @@
 |
 */
 
-$api = $app->make(Dingo\Api\Routing\Router::class);
+$router->get('/hello', function () {
+    return 'Hello World';
+});
+
+$api = $router->make(Dingo\Api\Routing\Router::class);
 
 $api->version('v1', function ($api) {
     $api->post('/auth/login', [
